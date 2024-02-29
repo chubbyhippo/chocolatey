29d905e42414ca9937a493fef17c4c51a5f95d41
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-29 09:33:13 +0700

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,30 @@
+# install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# install programs
+choco install autohotkey -y
+choco install cpu-z -y
+choco install gimp -y
+choco install git -y
+choco install golang -y
+choco install googlechrome -y
+choco install graalvm -y
+choco install inkscape -y
+choco install insomnia-rest-api-client -y
+choco install jetbrainstoolbox -y
+choco install jmc -y
+choco install nanazip -y
+choco install neovim -y
+choco install notepadplusplus -y
+choco install nvm -y
+choco install openjdk -y
+choco install python -y
+choco install rancher-desktop -y
+choco install sumatrapdf -y
+choco install tor-browser -y
+choco install virtualbox --params "/NoDesktopShortcut" -y
+choco install vlc -y
+choco install vscode --params "/NoDesktopIcon" -y
+choco install winscp -y
+
+# refresh
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
