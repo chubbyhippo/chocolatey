e4580239c5d4fc0e8ebd8cff22f27f29135cb633
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-03-16 23:16:13 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -29,6 +29,7 @@ choco install pandoc -y
 choco install python -y
 choco install rancher-desktop -y
 choco install rufus -y
+choco install ruby -y
 choco install sumatrapdf -y
 choco install tor-browser -y
 choco install virtualbox --params "/NoDesktopShortcut" -y
