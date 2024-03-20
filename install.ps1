3a917255d8adcded4e1982f5468ef0bc922e2136
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-03-20 14:08:53 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,42 +2,21 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # install programs
 
-# based
+# requireds
 choco install autohotkey -y
-choco install cmake -y
 
-
-choco install cpu-z -y
-choco install curl -y
-choco install gimp -y
+# terminal tools
 choco install git -y
-choco install golang -y
-choco install googlechrome -y
-choco install inkscape -y
-choco install insomnia-rest-api-client -y
-choco install jetbrainsmono -y
-
+choco install cmake -y
+choco install curl -y
 choco install jq -y
 choco install make -y
 choco install msys2 -y
-choco install nanazip -y
-
-
 choco install nvm -y
-choco install office365business --params "'/productid:O365ProPlusRetail /exclude:Access Groove Lync OneDrive OneNote Outlook Publisher /updates:FALSE /eula:FALSE'" -y
 choco install pandoc -y
-choco install python -y
-choco install rancher-desktop -y
+choco install golang -y
 choco install ruby -y
-choco install rufus -y
-choco install sumatrapdf -y
-choco install tor-browser -y
-choco install virtualbox --params "/NoDesktopShortcut" -y
-choco install vlc -y
-
-choco install wget -y
-choco install winscp -y
-choco install zoomit -y
+choco install python -y
 
 # java
 choco install openjdk -y
@@ -53,6 +32,27 @@ choco install jetbrainstoolbox -y
 choco install neovim -y
 choco install notepadplusplus -y
 choco install vscode --params "/NoDesktopIcon" -y
+
+# API testing tools
+choco install insomnia-rest-api-client -y
+
+# Apps
+choco install cpu-z -y
+choco install gimp -y
+choco install googlechrome -y
+choco install inkscape -y
+choco install jetbrainsmono -y
+choco install nanazip -y
+choco install rancher-desktop -y
+choco install rufus -y
+choco install sumatrapdf -y
+choco install tor-browser -y
+choco install virtualbox --params "/NoDesktopShortcut" -y
+choco install vlc -y
+choco install wget -y
+choco install winscp -y
+choco install zoomit -y
+choco install office365business --params "'/productid:O365ProPlusRetail /exclude:Access Groove Lync OneDrive OneNote Outlook Publisher /updates:FALSE /eula:FALSE'" -y
 
 # refresh
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
