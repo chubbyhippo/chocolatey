70abbb15c956b1723ea1ae80e9fd4f3edd4dfdd0
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-08-31 16:31:21 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,7 @@
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-# install programs
 
-# requireds
-choco install autohotkey -y
+# install programs
 
 # cmd tools
 choco install cmake -y
